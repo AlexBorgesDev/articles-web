edef6a194459de3c9462c939e47be3e9710170d1
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-31 10:51:50 -0300

    Created Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <section className="h-16 bg-surface">
-      <header className="w-full h-full max-w-7xl ml-auto mr-auto px-6 flex items-center">
+      <header className="w-full h-full max-w-7xl mx-auto px-6 flex items-center">
         <Link href="/">
           <a>
             <h2 className="text-2xl font-bold">Articles</h2>
