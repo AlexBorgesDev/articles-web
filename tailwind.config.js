a12dfc145aabac9a3a3406062e68bd34f5f7072d
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-31 14:15:25 -0300

    Changed tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        bg: "#101014",
-        surface: "#16161E",
-        "surface-two": "#1A1B26",
+        bg: '#101014',
+        surface: '#16161E',
+        'surface-two': '#1A1B26',
       },
     },
     fontFamily: {
-      sans: ["Roboto", "-apple-system", "sans-serif"],
+      sans: ['Roboto', 'ui-sans-serif', '-apple-system', 'sans-serif'],
     },
   },
   plugins: [],
