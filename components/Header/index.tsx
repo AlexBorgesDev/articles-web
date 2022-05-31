ceb56ed0ea50060ab32cf9ae243bb15489c7603f
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-05-31 16:36:54 -0300

    Changed Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,36 @@
 import Link from 'next/link';
 
+import { useEffect, useRef } from 'react';
+
 const Header = () => {
+  const ref = useRef<HTMLElement>(null);
+  const oldScrollY = useRef(0);
+
+  useEffect(() => {
+    function scrollEvent() {
+      if (window.scrollY < oldScrollY.current && ref.current) {
+        ref.current.classList.remove('top-[-4rem]');
+        ref.current.classList.add('top-0');
+      } else if (ref.current) {
+        ref.current.classList.add('top-[-4rem]');
+        ref.current.classList.remove('top-0');
+      }
+
+      oldScrollY.current = window.scrollY;
+    }
+
+    document.addEventListener('scroll', scrollEvent);
+
+    return () => {
+      document.removeEventListener('scroll', scrollEvent);
+    };
+  }, []);
+
   return (
-    <section className="h-16">
+    <section
+      ref={ref}
+      className="h-16 z-10 sticky bg-bg transition-all top-[-4rem]"
+    >
       <header className="w-full h-full max-w-7xl mx-auto px-6 flex items-center">
         <Link href="/">
           <a>
