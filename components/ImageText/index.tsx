04bf4c33b920998c96a8df3c13405fe015aa9000
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-17 18:13:14 -0300

    ImageText component created

--- components/ImageText/index.tsx
+++ components/ImageText/index.tsx
@@ -1,10 +1,8 @@
-import Image from 'next/image';
+/* eslint-disable @next/next/no-img-element */
 
 type ImageTextProps = {
   src: string;
   alt: string;
-  width: number;
-  height: number;
   description?: string;
 };
 
@@ -12,13 +10,14 @@ const ImageText = (props: ImageTextProps) => {
   return (
     <figure className="w-full pt-5 mb-12">
       <div className="w-fit mx-auto">
-        <Image
-          src={props.src}
-          alt={props.alt}
-          width={props.width}
-          height={props.height}
-          loading="lazy"
-        />
+        <picture>
+          <source srcSet={props.src} />
+          <img
+            src={props.src}
+            alt={props.alt}
+            className="max-h-[45rem] object-cover object-center"
+          />
+        </picture>
       </div>
 
       <figcaption className="text-sm text-gray-400 font-medium text-center pt-1">
