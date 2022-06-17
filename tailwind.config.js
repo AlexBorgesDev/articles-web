b6ea25f7de8281e06bffa7a5808718623eb9ca5d
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-17 18:12:08 -0300

    Changed Tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
     extend: {
       colors: {
         bg: '#101014',
+        accent: '#449DAB',
         primary: '#374F8F',
         surface: '#16161E',
         'blue-dark': '#2A2B3A',
         'surface-two': '#1A1B26',
+      },
+      fontSize: {
+        '4.5xl': ['2.5rem', { lineHeight: '3.438rem' }],
       },
     },
     fontFamily: {
