bae0ac2c6d70618ec27288bdcccfdf03135d2332
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-17 18:12:53 -0300

    Created the Paragraph component

--- components/Paragraph/index.tsx
+++ components/Paragraph/index.tsx
@@ -1,6 +1,4 @@
-import { ReactNode } from 'react';
-
-type ParagraphProps = { children: ReactNode };
+type ParagraphProps = { children: string };
 
 const Paragraph = ({ children }: ParagraphProps) => {
   return <p className="text-xl text-gray-300 opacity-95 mb-8">{children}</p>;
