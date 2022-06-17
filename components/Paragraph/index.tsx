654588be95daed8d66f94732f6b6ddcde43d32e4
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-17 19:04:04 -0300

    Changed the Paragraph component

--- components/Paragraph/index.tsx
+++ components/Paragraph/index.tsx
@@ -1,7 +1,11 @@
 type ParagraphProps = { children: string };
 
 const Paragraph = ({ children }: ParagraphProps) => {
-  return <p className="text-xl text-gray-300 opacity-95 mb-8">{children}</p>;
+  return (
+    <p className="text-lg tracking-[0.2px] font-medium sm:text-xl text-gray-300 opacity-95 mb-8">
+      {children}
+    </p>
+  );
 };
 
 export default Paragraph;
