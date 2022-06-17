0c9ccd2b0c1f1c6c30d2aa18778b823d6162e9a9
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-17 18:47:59 -0300

    Updated types for posts

--- @types/posts.d.ts
+++ @types/posts.d.ts
@@ -11,3 +11,21 @@ interface ResponsePosts {
   data: HomePost[];
   total: number;
 }
+
+type PostDataTypes = 'text' | 'code' | 'image' | 'subtitle';
+
+interface PostData {
+  tag: PostDataTypes;
+  data: string;
+  index: number;
+  picture?: { filename: string; description: string };
+}
+
+interface Post {
+  slug: string;
+  title: string;
+  banner: { filename: string; description: string };
+  data: PostData[];
+  owner: { firstName: string; lastName: string };
+  createdAt: string;
+}
