16e8a73d14bb84c70799e052febb6b73ec125e7f
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-17 18:47:09 -0300

    Changed the Presentation component

--- components/Presentation/index.tsx
+++ components/Presentation/index.tsx
@@ -2,12 +2,12 @@ import { memo } from 'react';
 
 const Presentation = () => {
   return (
-    <section className="w-full py-28 bg-surface-two">
+    <section className="w-full px-4 sm:py-28 py-20 bg-surface-two">
       <h1 className="mx-auto mb-5 text-center text-5xl font-bold font-['Fira_Code']">
         Articles
       </h1>
 
-      <h2 className="mx-auto max-w-2xl text-center text-2xl font-medium font-['Fira_Code']">
+      <h2 className="mx-auto max-w-2xl text-center text-base sm:text-2xl font-medium font-['Fira_Code']">
         Articles is an open platform where you can read and share your writings
         on any topic.
       </h2>
