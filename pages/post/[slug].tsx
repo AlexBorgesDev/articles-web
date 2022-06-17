c6268db6e18df521203d1bd0779e81fbd69839f6
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2022-06-17 19:08:54 -0300

    Changed post page by slug

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,66 +1,79 @@
-import type { NextPage } from 'next';
+import 'moment/locale/pt-br';
+
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
+import moment from 'moment';
 
 import Header from '../../components/Header';
 import ImageText from '../../components/ImageText';
 import Paragraph from '../../components/Paragraph';
 
-const Post: NextPage = () => {
+import api, { baseFilesURL } from '../../services/api';
+
+moment.locale('pt-br');
+
+const Post: NextPage<Post> = ({ banner, data, title, createdAt }) => {
   return (
     <div>
+      <Head>
+        <title>{title} - Articles</title>
+      </Head>
+
       <Header />
 
-      <section className="w-full h-[28rem] relative">
+      <section className="w-full h-72 sm:h-[28rem] relative">
         <Image
-          src="https://images.unsplash.com/photo-1611329532992-0b7ba27d85fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-          alt="Montanha"
+          src={`${baseFilesURL}${banner.filename}`}
+          alt={banner.description}
           layout="fill"
-          className="object-cover"
+          className="object-cover object-center"
         />
       </section>
 
-      <main className="w-full max-w-[53rem] mx-auto my-16 px-6">
-        <h1 className="text-4.5xl text-gray-300 opacity-95 mx-auto text-center font-bold">
-          Prisma: uma das melhores coisas que já aconteceu no ecossistema?
+      <main className="w-full max-w-[53rem] mx-auto my-16 px-5 sm:px-6">
+        <h1 className="text-3.2xl sm:text-4.5xl text-gray-300 opacity-95 mx-auto text-center font-bold">
+          {title}
         </h1>
 
         <p className="mt-4 text-base text-accent text-center font-semibold">
-          31 de Maio de 2022
+          {moment(createdAt).format('LL')}
         </p>
 
         <div className="w-full mt-16">
-          <Paragraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
-            placeat eaque deleniti tempora harum facere sint quam, dolores id
-            quaerat mollitia aperiam possimus labore ab? Quasi quaerat expedita
-            eos quam.
-          </Paragraph>
+          {data.map((value, index) => {
+            if (value.tag === 'image' && value.picture) {
+              return (
+                <ImageText
+                  key={index}
+                  src={`${baseFilesURL}${value.picture.filename}`}
+                  alt={value.picture.description}
+                  description={value.data}
+                />
+              );
+            }
 
-          <Paragraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
-            placeat eaque deleniti tempora harum facere sint quam, dolores id
-            quaerat mollitia aperiam possimus labore ab? Quasi quaerat expedita
-            eos quam.
-          </Paragraph>
-
-          <ImageText
-            src="https://images.unsplash.com/photo-1653503547900-78720b703a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-            alt="House"
-            width={488}
-            height={568}
-            description="Unsplash image"
-          />
-
-          <Paragraph>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
-            placeat eaque deleniti tempora harum facere sint quam, dolores id
-            quaerat mollitia aperiam possimus labore ab? Quasi quaerat expedita
-            eos quam.
-          </Paragraph>
+            return <Paragraph key={index}>{value.data}</Paragraph>;
+          })}
         </div>
       </main>
     </div>
   );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get<ResponsePosts>('post');
+
+  return {
+    paths: data.data.map(({ slug }) => ({ params: { slug } })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Post> = async context => {
+  const { data } = await api.get<Post>(`post/${context.params?.slug}`);
+
+  return { props: data };
 };
 
 export default Post;
